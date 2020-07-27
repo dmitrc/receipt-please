9a793e277db8c1e423e651562a54b82666209d92
Dmitrii Cucleschin <dmitrii.cucleschin@gmail.com>
2020-07-27 15:59:20 -0700

    improve quality of client-side JS

--- public/main.js
+++ public/main.js
@@ -1,21 +1,42 @@
 const $ = s => document.querySelector(s);
 const $$ = s => document.querySelectorAll(s);
+const ce = e => document.createElement(e);
 
-var maxW = 384;
-var maxH = 5000;
-var curY = 0;
-var curH = 0;
+const maxW = 384;
+const maxH = 5000;
+const cursorAllowance = 2;
 
-var canvasWrapper = $('#previewWrap');
+let curY = 0;
+let curH = 0;
+let isDrawingMode = false;
+let isDrawing = false;
+let mouse = { x: 0, y: 0 };
+let lastMouse = { x: 0, y: 0};
 
-var canvas = $('#previewCanvas');
-var ctx = canvas.getContext('2d');
+const preview = $('.preview');
+const canvasWrapper = $('#previewWrap');
+const canvas = $('#previewCanvas');
+const previewCursor = $('#previewCursor');
+const previewHeight = $('#previewHeight');
+const printBtn = $('#printBtn');
+const clearBtn = $('#clearBtn');
+const textBox = $('#textBox');
+const fontBox = $('#fontBox');
+const sizeBox = $('#sizeBox');
+const textBtn = $('#textBtn');
+const listBtn = $('#listBtn');
+const marginBox = $('#marginBox');
+const marginBtn = $('#marginBtn');
+const imageBox = $('#imageBox');
+const dithAlgo = $('#dithAlgo');
+const imageBtn = $('#imageBtn');
+const drawStartBtn = $('#drawStartBtn');
+const drawEndBtn = $('#drawEndBtn');
+
+const ctx = canvas.getContext('2d');
 canvas.width = maxW;
 canvas.height = maxH;
 
-var cursorAllowance = 2;
-var previewCursor = $('#previewCursor');
-var previewHeight = $('#previewHeight');
 function setCurrentHeight(y, force = false) {
     y = Math.max(y, 0);
     y = Math.min(canvas.height, y);
@@ -33,12 +54,12 @@ function setCurrentHeight(y, force = false) {
 }
 
 function updateCursors() {
-    var scale = canvas.width / canvasWrapper.clientWidth;
+    const scale = canvas.width / canvasWrapper.clientWidth;
 
     previewCursor.style.top = `${curY / scale}px`;
     previewHeight.style.top = `${curH / scale}px`;
 
-    var wrapperHeight = Math.max(preview.clientHeight, (curH / scale) + cursorAllowance);
+    const wrapperHeight = Math.max(preview.clientHeight, (curH / scale) + cursorAllowance);
     canvasWrapper.style.maxHeight = `${wrapperHeight}px`;
 }
 
@@ -46,35 +67,35 @@ function def(x, d) {
     return typeof(x) == 'undefined' ? d : x;
 }
 
-function wrapText(text, settings) {
+function renderText(text, settings) {
     settings = settings || {};
 
-    var x = def(settings.x, 0);
-    var y = def(settings.y, curY);
-    var w = def(settings.w, maxW);
-    var fontName = def(settings.fontName, 'Arial');
-    var fontSize = def(settings.fontSize, 28);
-    var lineHeightRatio = def(settings.lineHeightRatio, 1.2);
-    var useSpace = def(settings.useSpace, true);
-    var updateHeight = def(settings.updateHeight, true)
+    let y = def(settings.y, curY);
+    const x = def(settings.x, 0);
+    const w = def(settings.w, maxW);
+    const fontName = def(settings.fontName, 'Arial');
+    const fontSize = def(settings.fontSize, 28);
+    const lineHeightRatio = def(settings.lineHeightRatio, 1.2);
+    const useSpace = def(settings.useSpace, true);
+    const updateHeight = def(settings.updateHeight, true)
 
     ctx.font = `${fontSize}px ${fontName}`;
     ctx.textBaseline = 'top';
     ctx.fillStyle = 'black';
     
-    var lineHeight = Math.ceil(fontSize * lineHeightRatio);
-    var paragraphs = text.split('\n');
-    var line = '';
+    const lineHeight = Math.ceil(fontSize * lineHeightRatio);
+    const paragraphs = text.split('\n');
+    let line = '';
 
-    for (var i = 0; i < paragraphs.length; i++) {
-	    var breakChar = useSpace ? ' ' : '';
-        var words = paragraphs[i].split(breakChar);
+    for (let i = 0; i < paragraphs.length; i++) {
+	    const breakChar = useSpace ? ' ' : '';
+        const words = paragraphs[i].split(breakChar);
 
-        for (var n = 0; n < words.length; n++)
+        for (let n = 0; n < words.length; n++)
         {
-            var testLine = line + words[n] + breakChar;
-            var metrics = ctx.measureText(testLine);
-            var testWidth = metrics.width;
+            const testLine = line + words[n] + breakChar;
+            const metrics = ctx.measureText(testLine);
+            const testWidth = metrics.width;
 
             if (testWidth > w && n > 0)
             {
@@ -97,9 +118,8 @@ function wrapText(text, settings) {
     return y;
 }
 
-var dithAlgo = $('#dithAlgo');
 function ditherImage(imageCanvas, imageCtx) {
-    var opts = {
+    const opts = {
         colors: 2,
         method: 2,
         boxSize: [8, 8],
@@ -116,67 +136,67 @@ function ditherImage(imageCanvas, imageCtx) {
         colorDist: 'euclidean'
     };
 
-    var w = imageCanvas.width;
-    var h = imageCanvas.height;
+    const w = imageCanvas.width;
+    const h = imageCanvas.height;
 
-    var q = new RgbQuant(opts);
+    const q = new RgbQuant(opts);
 
-    var ditherResult = q.reduce(imageCanvas);
+    const ditherResult = q.reduce(imageCanvas);
 
-    var imgData = imageCtx.getImageData(0, 0, w, h);
+    const imgData = imageCtx.getImageData(0, 0, w, h);
     imgData.data.set(ditherResult);
     imageCtx.putImageData(imgData, 0, 0);
 
     return imageCanvas;
 }
 
-function drawImage(image, settings) {
+function renderImage(image, settings) {
     settings = settings || {};
 
-    var x = def(settings.x, 0);
-    var y = def(settings.y, curY);
-    var updateHeight = def(updateHeight, true);
+    const x = def(settings.x, 0);
+    const y = def(settings.y, curY);
+    const updateHeight = def(updateHeight, true);
 
-    var w = image.width;
-    var h = image.height;
+    const w = image.width;
+    const h = image.height;
 
-    var adjustedW = maxW;
-    var adjustedH = h * (adjustedW / w);
+    const adjustedW = maxW;
+    const adjustedH = h * (adjustedW / w);
 
-    var imageCanvas = document.createElement('canvas');
+    const imageCanvas = ce('canvas');
     imageCanvas.width = adjustedW;
     imageCanvas.height = adjustedH;
 
-    var imageCtx = imageCanvas.getContext('2d');
+    const imageCtx = imageCanvas.getContext('2d');
     imageCtx.drawImage(image, 0, 0, adjustedW, adjustedH);
 
-    var ditheredImage = ditherImage(imageCanvas, imageCtx);
+    const ditheredImage = ditherImage(imageCanvas, imageCtx);
     ctx.drawImage(ditheredImage, x, y);
 
-    var newH = (y + adjustedH);
+    const newH = (y + adjustedH);
     updateHeight && setCurrentHeight(newH);
     return newH;
 }
 
-function addListItem(text, settings) {
+function renderListItem(text, settings) {
     settings = settings || {};
 
-    var y = def(settings.y, curY);
-    var fontName = def(settings.fontName, 'Arial');
-    var fontSize = def(settings.fontSize, 28);
-    var iconSize = def(settings.iconSize, 32);
-    var lineHeightRatio = def(settings.lineHeightRatio, 1.2);
-    var useSpace = def(settings.useSpace, true);
-    var updateHeight = def(settings.updateHeight, true)
+    const y = def(settings.y, curY);
+    const fontName = def(settings.fontName, 'Arial');
+    const fontSize = def(settings.fontSize, 28);
+    const iconSize = def(settings.iconSize, 32);
+    const lineHeightRatio = def(settings.lineHeightRatio, 1.2);
+    const useSpace = def(settings.useSpace, true);
+    const updateHeight = def(settings.updateHeight, true)
 
-    var h1 = wrapText('☐', {
+    const h1 = renderText('☐', {
         fontName: 'Arial',
         fontSize: iconSize,
         lineHeight: 1.0,
         updateHeight: false
     });
 
-    var h2 = wrapText(text, { 
+    const h2 = renderText(text, { 
         x: 40,
         w: maxW - 40,
         fontName: fontName,
@@ -186,7 +206,7 @@ function addListItem(text, settings) {
         updateHeight: false
     });
 
-    var newH = Math.max(h1, h2);
+    const newH = Math.max(h1, h2);
 
     ctx.fillStyle = 'rgba(0, 0, 0, 0.3)';
     ctx.fillRect(0, newH, maxW, 1);
@@ -197,42 +217,32 @@ function addListItem(text, settings) {
     return newH;
 }
 
-function printBlob(blob) {
-    const formData = new FormData();
-    formData.append('blob', blob);
-
-    const opts = {
-        method: 'POST',
-        body: formData
-    };
-
-    fetch('/print', opts)
-        .then(res => console.log)
-        .catch(err => console.error);
-}
-
-function generateAndPrintBlob(trimH = curH) {
+function print(trimH = curH) {
     const exportData = ctx.getImageData(0, 0, maxW, trimH);
 
-    var exportCanvas = document.createElement('canvas');
+    const exportCanvas = ce('canvas');
     exportCanvas.width = maxW;
     exportCanvas.height = trimH;
     
-    var exportCtx = exportCanvas.getContext('2d');
+    const exportCtx = exportCanvas.getContext('2d');
     exportCtx.putImageData(exportData, 0, 0);
     
     exportCanvas.toBlob(blob => {
-        printBlob(blob);
+        const formData = new FormData();
+        formData.append('blob', blob);
+
+        const opts = {
+            method: 'POST',
+            body: formData
+        };
+
+        fetch('/print', opts)
+            .then(res => console.log)
+            .catch(err => console.error);
     });
 }
 
-var printBtn = $('#printBtn');
-printBtn.onclick = () => {
-    generateAndPrintBlob();
-};
-
-var clearBtn = $('#clearBtn');
-clearBtn.onclick = () => {
+function clear() {
     ctx.fillStyle = '#fff';
     if (curY < curH) {
         ctx.fillRect(0, curY, maxW, curH - curY);
@@ -242,173 +252,124 @@ clearBtn.onclick = () => {
         ctx.fillRect(0, 0, maxW, curH);
         setCurrentHeight(0, true);
     }
-};
+}
 
-var textBox = $('#textBox');
-var fontBox = $('#fontBox');
-var sizeBox = $('#sizeBox');
-
-var textBtn = $('#textBtn');
-textBtn.onclick = () => {
-    var text = textBox.value;
+function addText() {
+    const text = textBox.value;
     if (text) {
-        var fontSetting = fontBox.value || undefined;
-        var sizeSetting = parseInt(sizeBox.value);
+        const fontSetting = fontBox.value || undefined;
+        const sizeSetting = parseInt(sizeBox.value);
         
         if (isNaN(sizeSetting)) {
             sizeSetting = undefined;
         }
 
-        wrapText(text, { fontName: fontSetting, fontSize: sizeSetting });
+        renderText(text, { fontName: fontSetting, fontSize: sizeSetting });
 
         textBox.value = '';
         textBox.focus();
     }
-};
+}
 
-var listBtn = $('#listBtn');
-listBtn.onclick = () => {
-    var text = textBox.value;
+function addList() {
+    const text = textBox.value;
     if (text) {
-        var fontSetting = fontBox.value || undefined;
-        var sizeSetting = parseInt(sizeBox.value);
+        const fontSetting = fontBox.value || undefined;
+        const sizeSetting = parseInt(sizeBox.value);
         
         if (isNaN(sizeSetting)) {
             sizeSetting = undefined;
         }
 
         items = text.split('\n');
-        for (var item of items) {
-            addListItem(item, { fontName: fontSetting, fontSize: sizeSetting });
+        for (const item of items) {
+            renderListItem(item, { fontName: fontSetting, fontSize: sizeSetting });
         }
 
         textBox.value = '';
         textBox.focus();
     }
-};
+}
 
-var marginBox = $('#marginBox');
-
-var marginBtn = $('#marginBtn');
-marginBtn.onclick = () => {
-    var marginSetting = parseInt(marginBox.value);
+function addMargin() {
+    const marginSetting = parseInt(marginBox.value);
     if (!isNaN(marginSetting)) {
         setCurrentHeight(curY + marginSetting);
     }
-};
+}
 
-var imageBox = $('#imageBox');
-var imageBtn = $('#imageBtn');
-imageBtn.onclick = () => {
-    var files = imageBox.files;
+function addImage() {
+    const files = imageBox.files;
     if (files.length > 0) {
-        var file = files[0];
+        const file = files[0];
 
-        var img = new Image();
+        const img = new Image();
         img.onload = () => {
-            drawImage(img);
+            renderImage(img);
             URL.revokeObjectURL(file);
         }
 
         img.src = URL.createObjectURL(file);
     }
-};
+}
 
-var isDrawingMode = false;
-var isDrawing = false;
-
-var mouse = { x: 0, y: 0 };
-var lastMouse = { x: 0, y: 0};
-
-var preview = document.querySelector('.preview');
-var drawStartBtn = $('#drawStartBtn');
-drawStartBtn.onclick = () => {
+function enableDraw() {
     isDrawingMode = true;
 
     canvas.classList.add('draw');
     drawStartBtn.classList.add('hide');
     drawEndBtn.classList.remove('hide');
+}
 
-    if (curH < preview.clientHeight) {
-        canvasWrapper.style.maxHeight = `${preview.clientHeight}px`;
-    }
-};
-
-var drawEndBtn = $('#drawEndBtn');
-drawEndBtn.onclick = () => {
+function disableDraw() {
     isDrawingMode = false;
 
     canvas.classList.remove('draw');
     drawEndBtn.classList.add('hide');
     drawStartBtn.classList.remove('hide');
-};
+}
 
-canvas.onmousedown = (e) => {
+function startDraw(x, y) {
     if (isDrawingMode) {
-        var canvas = {
+        const canvas = {
             x: preview.offsetLeft,
             y: preview.offsetTop - preview.scrollTop
         };
 
-        lastMouse.x = mouse.x = parseInt(e.pageX-canvas.x);
-	    lastMouse.y = mouse.y = parseInt(e.pageY-canvas.y);
+        const touch = e.targetTouches[0];
+
+        lastMouse.x = mouse.x = parseInt(x-canvas.x);
+	    lastMouse.y = mouse.y = parseInt(y-canvas.y);
         isDrawing = true
     }
 }
 
-canvas.ontouchstart = (e) => {
-    if (isDrawingMode) {
-        var canvas = {
-            x: preview.offsetLeft,
-            y: preview.offsetTop - preview.scrollTop
-        };
-
-        var touch = e.targetTouches[0];
-
-        lastMouse.x = mouse.x = parseInt(touch.pageX-canvas.x);
-	    lastMouse.y = mouse.y = parseInt(touch.pageY-canvas.y);
-        isDrawing = true
-    }
-}
-
-canvas.onmouseup = (e) => {
+function endDraw() {
     if (isDrawingMode) {
         isDrawing = false;
     }
 }
 
-canvas.ontouchend = (e) => {
-    if (isDrawingMode) {
-        isDrawing = false;
-    }
-}
-
-canvas.ontouchcancel = (e) => {
-    if (isDrawingMode) {
-        isDrawing = false;
-    }
-}
-
-canvas.onmousemove = (e) => {
-    var c = {
+function moveDraw(x, y) {
+    const c = {
         x: preview.offsetLeft,
         y: preview.offsetTop - preview.scrollTop,
         scale: canvas.width / canvasWrapper.clientWidth
     };
 
-    mouse.x = parseInt(e.pageX - c.x);
-    mouse.y = parseInt(e.pageY - c.y);
+    mouse.x = parseInt(x - c.x);
+    mouse.y = parseInt(y - c.y);
 
     if (isDrawingMode && isDrawing) {
         ctx.strokeStyle = 'black';
         ctx.lineWidth = 2;
 
-        var scaledLastMouse = { 
+        const scaledLastMouse = { 
             x: lastMouse.x * c.scale,
             y: lastMouse.y * c.scale
         };
 
-        var scaledMouse = { 
+        const scaledMouse = { 
             x: mouse.x * c.scale,
             y: mouse.y * c.scale
         };
@@ -428,49 +389,39 @@ canvas.onmousemove = (e) => {
     }
 }
 
-canvas.ontouchmove = (e) => {
-    var c = {
-        x: preview.offsetLeft,
-        y: preview.offsetTop - preview.scrollTop,
-        scale: canvas.width / canvasWrapper.clientWidth
-    };
+function addHandlers() {
+    window.onresize = updateCursors;
 
-    var touch = e.targetTouches[0];
+    printBtn.onclick = print;
+    clearBtn.onclick = clear;
+    textBtn.onclick = addText;
+    listBtn.onclick = addList;
+    marginBtn.onclick = addMargin;
+    imageBtn.onclick = addImage;
+    drawStartBtn.onclick = enableDraw;
+    drawEndBtn.onclick = disableDraw;
 
-    mouse.x = parseInt(touch.pageX - c.x);
-    mouse.y = parseInt(touch.pageY - c.y);
-
-    if (isDrawingMode && isDrawing) {
-        ctx.strokeStyle = 'black';
-        ctx.lineWidth = 2;
-
-        var scaledLastMouse = { 
-            x: lastMouse.x * c.scale,
-            y: lastMouse.y * c.scale
-        };
-
-        var scaledMouse = { 
-            x: mouse.x * c.scale,
-            y: mouse.y * c.scale
-        };
-
-        ctx.beginPath();
-        ctx.moveTo(scaledLastMouse.x, scaledLastMouse.y);
-        ctx.lineTo(scaledMouse.x, scaledMouse.y);
-        ctx.closePath();
-        ctx.stroke();
-
-        if (scaledMouse.y > curH) {
-            setCurrentHeight(scaledMouse.y + 2);
-        }
-
-        lastMouse.x = mouse.x;
-        lastMouse.y = mouse.y;
-
-        e.preventDefault();
+    canvas.onmousedown = (e) => {
+        startDraw(e.pageX, e.pageY);
+    }
+    
+    canvas.ontouchstart = (e) => {
+        const touch = e.touches[0];
+        startDraw(touch.pageX, touch.pageY);
+    }
+    
+    canvas.onmouseup = endDraw;
+    canvas.ontouchend = endDraw;
+    canvas.ontouchcancel = endDraw;
+    
+    canvas.onmousemove = (e) => {
+        moveDraw(e.pageX, e.pageY);
+    }
+    
+    canvas.ontouchmove = (e) => {
+        const touch = e.touches[0];
+        moveDraw(touch.pageX, touch.pageY);
     }
 }
 
-window.onresize = () => {
-    updateCursors();
-}
+addHandlers();
